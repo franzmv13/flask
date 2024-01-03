697800d72fc41d1520786f04330979485975b57d
franzmv13 <202080210@psu.palawan.edu.ph>
2024-01-03 20:40:24 +0800

    utilized mysql, and add get_actors fuction

--- api.py
+++ api.py
@@ -1,11 +1,37 @@
 
-from flask import Flask
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello, World!</p>"
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+
+
+from flask import *
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# Required
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "franz"
+app.config["MYSQL_DB"] = "final"
+# Extra configs, optional:
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+
+mysql = MySQL(app)
+
+@app.route("/", methods=["GET"])
+def users():
+    cur = mysql.connection.cursor()
+    cur.execute("""SELECT * FROM final.address;""")
+    rv = cur.fetchall()
+    return make_response(jsonify(rv), 200)
 
 if __name__ == "__main__":
     app.run(debug=True)
