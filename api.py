512a0f028213cbd85a35f9d47f37ffa7c6ab7f49
franzmv13 <202080210@psu.palawan.edu.ph>
2024-01-04 12:30:30 +0800

    finalizing flask proj

--- api.py
+++ api.py
@@ -27,33 +27,33 @@ mysql = MySQL(app)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/manager", methods=["GET"])
+@app.route("/patients", methods=["GET"])
 def users():
     cur = mysql.connection.cursor()
-    cur.execute("""SELECT * FROM final.manager;""")
+    cur.execute("""SELECT * FROM final.patients;""")
     rv = cur.fetchall()
     return make_response(jsonify(rv), 200)
 
-@app.route("/manager/<int:id>", methods=["GET"])
+@app.route("/patients/<int:id>", methods=["GET"])
 def get_actor_by_id(id):
     cur = mysql.connection.cursor()
-    cur.execute("""SELECT * FROM final.manager WHERE `id` = {}""".format(id))
+    cur.execute("""SELECT * FROM final.patients WHERE `id` = {}""".format(id))
     rv = cur.fetchall()
     return make_response(jsonify(rv), 200)
 
-@app.route("/manager/<int:id>/agency", methods=["GET"])
+@app.route("/patients/<int:id>/agency", methods=["GET"])
 def get_agency_by_id(id):
     cur = mysql.connection.cursor()
-    cur.execute("""SELECT manager.first_name,manager.last_name, agency.agency_name
-                    FROM manager
-                    INNER JOIN agency on agency.id = manager.id
+    cur.execute("""SELECT patients.first_name,patients.last_name, agency.agency_name
+                    FROM patients
+                    INNER JOIN agency on agency.id = patients.id
                     WHERE agency.id = 2 {};
                 """.format(id))
     rv = cur.fetchall()
     return make_response(jsonify(rv), 200)
 
 
-@app.route("/manager", methods=["POST"])
+@app.route("/patients", methods=["POST"])
 def add_actor():
     cur = mysql.connection.cursor()
     info = request.get_json()
@@ -61,7 +61,7 @@ def add_actor():
     lastname = info["lname"]
     print(firstname , lastname)
     cur.execute(
-        """ INSERT INTO `final`.`manager` (`first_name`, `last_name`) VALUES (%s, %s)""",
+        """ INSERT INTO `final`.`patients` (`first_name`, `last_name`) VALUES (%s, %s)""",
         (firstname, lastname),
     )
     mysql.connection.commit()
@@ -70,20 +70,20 @@ def add_actor():
     cur.close()
     return make_response(
         jsonify(
-            {"message": "manager added successfully", "rows_affected": rows_affected}
+            {"message": "patients added successfully", "rows_affected": rows_affected}
         ),
         200,
     )
 
 
-@app.route("/manager/<int:id>", methods=["PUT"])
-def update_actor(id):
+@app.route("/patients/<int:id>", methods=["PUT"])
+def update_patients(id):
     cur = mysql.connection.cursor()
     info = request.get_json()
     firstname = info["fname"]
     lastname = info["lname"]
     cur.execute(
-        """UPDATE `final`.`manager` SET `first_name` = %s, `last_name` = %s WHERE (`id` = %s);
+        """UPDATE `final`.`patients` SET `first_name` = %s, `last_name` = %s WHERE (`id` = %s);
  """,
         (firstname, lastname, id),
     )
@@ -92,11 +92,24 @@ def update_actor(id):
     cur.close()
     return make_response(
         jsonify(
-            {"message": "actor updated successfully", "rows_affected": rows_affected}
+            {"message": "patients updated successfully", "rows_affected": rows_affected}
         ),
         200,
     )
 
 
+@app.route("/patients/<int:id>", methods=["DELETE"])
+def del_patients(id):
+    cur = mysql.connection.cursor()
+    cur.execute(""" DELETE FROM patients where id = %s """, (id,))
+    mysql.connection.commit()
+    rows_affected = cur.rowcount
+    cur.close()
+    return make_response(
+        jsonify(
+            {"message": "patients deleted successfully", "rows_affected": rows_affected}
+        ),
+        200,
+    )
 if __name__ == "__main__":
     app.run(debug=True)
